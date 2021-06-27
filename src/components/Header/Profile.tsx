71beb1f2c19edd732cc8e43e7cb7c9ba4b62119b
unknown <marcos.cesar@ccc.ufcg.edu.br>
2021-06-26 22:00:54 -0300

    Trabalhando com responsividade

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,15 +1,20 @@
 import { Flex, Box, Text, Avatar } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex align='center'>
-      <Box mr='4' textAlign='right'>
+      { showProfileData && (
+        <Box mr='4' textAlign='right'>
         <Text>Marcos Cesar</Text>
         <Text color='gray.300' fontSize='small'>
           marcos.cesar@sidia.com
         </Text>
       </Box>
-
+      )}
       <Avatar size='md' name='Marcos Cesar' src='https://avatars.githubusercontent.com/u/11970313?v=4' />
     </Flex>
   )
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,19 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react';
 import { Profile } from './Profile';
 import { NotificationsNav } from './NotificationsNav';
 import { SearchBox } from './SearchBox';
 import { Logo } from './Logo';
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext';
+import { RiMenuLine } from 'react-icons/ri';
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer();
+
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    md: true
+  });
+
   return (
     <Flex
       as='header'
@@ -16,13 +25,24 @@ export function Header() {
       px='6'
       align='center'
     >
+      { !isWideVersion && (
+        <IconButton 
+          icon={<Icon as={RiMenuLine} />}
+          fontSize='24'
+          variant='unstyled'
+          onClick={onOpen}
+          aria-label='Open navigation'
+          mr='2'
+        />
+      )}
+
       <Logo />
 
-      <SearchBox />
+      { isWideVersion && <SearchBox /> }
     
       <Flex align='center' ml='auto'>
         <NotificationsNav />
-        <Profile />
+        <Profile showProfileData={isWideVersion} />
       </Flex>
     </Flex>
   )
