8e5c2783bdaa194a94f773409e54ae8a47d3b8eb
unknown <marcos.cesar@ccc.ufcg.edu.br>
2021-06-27 21:34:12 -0300

    Fazendo o fetch dos dados usando Miragejs e React Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,12 +1,49 @@
-import { Box, Flex, Heading, Button, Icon, Text, Table, Thead, Tr, Th, Checkbox, Tbody, Td, useBreakpointValue } from '@chakra-ui/react';
+import { 
+  Box, 
+  Flex, 
+  Heading, 
+  Button, 
+  Icon, 
+  Text, 
+  Table, 
+  Thead, 
+  Tr, 
+  Th,
+  Checkbox,
+  Tbody,
+  Td,
+  useBreakpointValue,
+  Spinner,
+  Skeleton,
+  Stack
+} from '@chakra-ui/react';
+import Link from 'next/link';
+import { RiAddLine } from 'react-icons/ri';
+import { useQuery } from 'react-query';
+
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
-import { RiAddLine } from 'react-icons/ri';
-import React from 'react';
 import { Pagination } from '../../components/Pagination';
-import Link from 'next/link';
+import { api } from '../../services/api';
 
 export default function UserList() {
+  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
+    const { data } = await api.get('users')
+    
+    return data.users.map(user => {
+      return {
+        ...user,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      }
+    })
+  }, {
+    staleTime: 1000 * 5, // 5 seconds
+  });
+
   const isWideVersion = useBreakpointValue({
     base: false,
     md: true
@@ -21,7 +58,12 @@ export default function UserList() {
         <Sidebar />
         <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
           <Flex mb='8' justify='space-between' align='center'>
-            <Heading size='lg' fontWeight='normal'>Usuários</Heading>
+            <Heading size='lg' fontWeight='normal'>
+              Usuários
+
+              { !isLoading && isFetching && <Spinner size='sm' color='gray.500' ml='4'/> }
+            
+            </Heading>
             <Link href='/users/create' passHref>
               <Button
                 as='a'
@@ -35,33 +77,49 @@ export default function UserList() {
             </Link>
           </Flex>
         
-          <Table colorScheme='whiteAlpha'>
-            <Thead>
-              <Tr>
-                <Th px={['4', '4', '6']} color='gray.300' width='8'>
-                  <Checkbox colorScheme='pink' />
-                </Th>
-                <Th>Usuário</Th>
-                { isWideVersion && <Th>Data de cadastro</Th> }
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme='pink' />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>Marcos Cesar</Text>
-                    <Text fontSize='sm' color='gray.300'>marcos.cesar@sidia.com</Text>
-                  </Box>
-                </Td>
-                { isWideVersion && <Td>04 de Abril, 2020</Td> }
-              </Tr>
-            </Tbody>
-          </Table>
+          { isLoading ? (
+            <Stack >
+              <Skeleton height='40px' width='100%' startColor='gray.800' endColor='gray.900'/>
+              <Skeleton height='40px' width='100%' startColor='gray.800' endColor='gray.900'/>
+              <Skeleton height='40px' width='100%' startColor='gray.800' endColor='gray.900'/>
+            </Stack>
+          ) : error ? (
+            <Flex justify='center'>
+              <Text>Falha ao obter dados dos usuários</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme='whiteAlpha'>
+              <Thead>
+                <Tr>
+                  <Th px={['4', '4', '6']} color='gray.300' width='8'>
+                    <Checkbox colorScheme='pink' />
+                  </Th>
+                  <Th>Usuário</Th>
+                  { isWideVersion && <Th>Data de cadastro</Th> }
+                </Tr>
+              </Thead>
+              <Tbody>
+                { data.map(user => (
+                  <Tr key={user.id}>
+                    <Td px={['4', '4', '6']}>
+                      <Checkbox colorScheme='pink' />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight='bold'>{user.name}</Text>
+                        <Text fontSize='sm' color='gray.300'>{user.email}</Text>
+                      </Box>
+                    </Td>
+                    { isWideVersion && <Td>{user.createdAt}</Td> }
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
 
-          <Pagination />
+            <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
